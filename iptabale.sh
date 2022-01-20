556676a3b15d44562720becb38be13fe0e0567b6
Reza <66633502+Rezasalami@users.noreply.github.com>
2022-01-21 01:21:21 +0330

    Create iptabale.sh
    init

---
+++ iptabale.sh
@@ -0,0 +1,25 @@
+iptables --flush
+iptables --table nat --flush
+iptables --delete-chain
+#Allow loopback
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A OUTPUT -o lo -j ACCEPT
+#Allow established connections:
+iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+iptables -A OUTPUT -m conntrack --ctstate NEW,ESTABLISHED,RELATED -j ACCEPT
+
+iptables -t nat -A POSTROUTING -o ens160 -j MASQUERADE
+iptables -A FORWARD -i ens160 -j ACCEPT
+
+
+#DNS
+iptables -I INPUT -p udp -m udp --dport 53 -j ACCEPT 
+
+
+
+#ssh port
+iptables -I INPUT -s {Myip-in local pc not server}  -p tcp -m tcp --dport 22 -j ACCEPT
+
+
+
+iptables -A INPUT -j DROP
